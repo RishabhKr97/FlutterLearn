9c75ca29192cfb35166fb2a02dff89cdd613d4a3
Rishabh Kumar <rishabhkr1729@gmail.com>
2023-06-25 02:08:37 +0530

    Meals App - Add hero animation across screens

--- meals/lib/screens/meal_details.dart
+++ meals/lib/screens/meal_details.dart
@@ -51,12 +51,15 @@ class MealDetailsScreen extends ConsumerWidget {
       ),
       body: Column(
         children: [
-          FadeInImage(
-            placeholder: MemoryImage(kTransparentImage),
-            image: NetworkImage(meal.imageUrl),
-            fit: BoxFit.cover,
-            height: 300,
-            width: double.infinity,
+          Hero(
+            tag: meal.id,
+            child: FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 300,
+              width: double.infinity,
+            ),
           ),
           Expanded(
             child: SingleChildScrollView(
